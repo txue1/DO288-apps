53212f46ebf4155ab452a1f373f7f881bdf5cb4c
Student User <student@workstation.lab.example.com>
2020-09-07 04:07:36 +0000

    Fix more errors

--- movies/src/main/java/com/redhat/movies/MoviesController.java
+++ movies/src/main/java/com/redhat/movies/MoviesController.java
@@ -3,6 +3,7 @@ package com.redhat.movies;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
